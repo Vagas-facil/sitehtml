dc3f28e3482df77e35c7523774606efed59c4c4b
patrick-e <patrickp4k@hotmail.com>
2022-11-19 12:32:18 -0300

    codigo funcional

--- codigo/conexao.php
+++ codigo/conexao.php
@@ -1,10 +1,20 @@
 <?php
 
-$servername = "localhost";
+$servername = "localhost:3307"; // 3307 é a porta, se estiver em 3306 deixe apenas localhost que funcionara normalmente
 $username = "root";
 $password = "";
 $db = 'login';
 
-$mysqli = mysql_conect($servername,$username,$password, $db) or die( "Falha ao conectar");
-
-$conn->close();
+$mysqli = new mysqli($servername,$username,$password, $db);
+if(!$mysqli){
+    die("erro ao conectar: ". mysql_error());
+}
+// conexao com o banco de dados
+echo "cabecinha do meu pau fuck off";
+$sql = 'CREATE TABLE IF NOT EXISTS login(
+    Nome VARCHAR(50),
+    Email VARCHAR(50),
+    Senha VARCHAR(50));';
+$table = mysqli_query($mysqli,$sql);
+mysqli_error($mysqli);
+//criação de tabela caso n exista
--- codigo/request.php
+++ codigo/request.php
@@ -5,16 +5,12 @@ $nome = $_POST['nomec'];
 $email = $_POST['emailc'];
 $senha = $_POST['senhaclient'];
 $senhac = $_POST['senhac'];
-
-
-$sql = "CREATE TABLE  ximarao(
-    id INT(6) NOT NULL PRIMARY KEY,
-    nome VARCHAR(50),
-    email VARCHAR(50),
-    senha VARCHAR(50),
-    )";
-
-$db = "INSERT INTO login() VALUES($nome,$email,$senha)";
-mysqli_query($mysqli, $db) or die("erro na transferencia");
+//puxar dados do html 
+$db = "INSERT INTO login(Nome, Email, Senha) VALUES('$nome','$email','$senha')";
+if(!mysqli_query($mysqli, $db)){ 
+    die("erro na transferencia: ");
+}
+//inserção de dados do html na tabela criada anteriormente
+echo "funciona";
 mysqli_close($mysqli);
 ?>
