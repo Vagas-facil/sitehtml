edcf6dd05e96fb6205bed1d183ed87d68430ad88
Pedro Henrique Lourenço Ferezin <83138930+PedroHenrique-42@users.noreply.github.com>
2022-12-06 17:51:39 -0300

    Inserção dos dados no banco via PHP

--- codigo/request.php
+++ codigo/request.php
@@ -1,23 +1,50 @@
 <?php
-include("conexao.php");
+    include("conexao.php");
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin-Header: Content-Type, Accept');
+    header('Content-Type: application/json');
 
-$nome = $_POST['nomec'];
-$email = $_POST['emailc'];
-$senha = $_POST['senhaclient'];
-$senhac = $_POST['csenha'];
+    $json = file_get_contents('php://input');
+    //echo $json;
 
-//puxar dados do html 
-if($senhac == $senha){
-    $db = "INSERT INTO login(Nome, Email, Senha) VALUES('$nome','$email','$senha')";
-    if(!mysqli_query($mysqli, $db)){ 
-        die("erro na transferencia: ");
+    $dados = json_decode($json);
+    print_r($dados);
+
+    //puxar dados do html 
+    if($_POST['nomec'] != NULL){
+        $nome = $_POST['nomec'];
+        $email = $_POST['emailc']; //or $json['email']) ;
+        $senha = $_POST['senhaclient']; //or $json['password']);
+        $senhac = $_POST['csenha']; //or $json['password']);
+    
+        if($senhac == $senha){
+            $db = "INSERT INTO login(Nome, Email, Senha) VALUES('$nome','$email','$senha')";
+            if(!mysqli_query($mysqli, $db)){ 
+                die("erro na transferencia: ");
+            }
+            //inserção de dados do html na tabela criada anteriormente
+            echo "funciona";
+            mysqli_close($mysqli);
+            header('location:../index.html');
+        } 
     }
-    //inserção de dados do html na tabela criada anteriormente
-    echo "funciona";
-    mysqli_close($mysqli);
-    header('location:../index.html');
-}
-else{
-    echo '      algo errado ocorreu ao cadastrar';
-}
+    else if($dados != NULL) {
+        // Pegando os dados da requisição e enviando para o DB
+        $nomef = $dados->username; 
+        $senhaf = $dados->password; 
+        $senhacf = $dados->password; 
+        $emailf = $dados->email; 
+
+        $db = "INSERT INTO login(Nome, Email, Senha) VALUES('$nomef','$emailf','$senhaf')";
+        if(!mysqli_query($mysqli, $db)){ 
+            die("Erro na transferencia: ");
+        }
+        //inserção de dados do html na tabela criada anteriormente
+        echo "Funciona Flutter";
+        mysqli_close($mysqli);
+    }
+    else{
+        echo "Algo errado ocorreu ao cadastrar";
+    }
 ?>
